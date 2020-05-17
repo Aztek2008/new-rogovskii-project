9daeaa1f090727704dc9213882bcbbaefc5f38fb
Oleksii Skopiuk <aztek2008@gmail.com>
2020-05-17 13:37:36 +0300

    Specify css for collapse left menu

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,8 +2,12 @@ const strap = document.querySelector('.main-area__strap');
 const leftMenu = document.querySelector('.left-side__section');
 console.log(strap);
 
-strap.addEventListener('click', collapseLeftMenu)
+strap.addEventListener('click', collapseLeftMenu);
 
 function collapseLeftMenu(event) {
-  leftMenu.classList.toggle('collapse')
+  if (leftMenu.dataset.id === 'collapse-to-left') {
+    leftMenu.dataset.id = '';
+  } else {
+    leftMenu.dataset.id = 'collapse-to-left';
+  }
 }
